8804fd36fc2b14cf203a769c8608095d23466d01
samiullah113 <18134119-002@uog.edu.pk>
2023-01-09 12:55:09 +0500

    testing app

--- config/wdio.android.app.conf.ts
+++ config/wdio.android.app.conf.ts
@@ -21,12 +21,12 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_3_10.0',
-        'appium:platformVersion': '10.0',
+        'appium:deviceName': 'Google Pixel 3',
+        'appium:platformVersion': '10',
         'appium:orientation': 'PORTRAIT',
         'appium:automationName': 'UiAutomator2',
         // The path to the app
-        'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.4.0.apk'),
+        'appium:app': join(process.cwd(), 'apps/Android-NativeDemoApp-0.4.0.apk'),
         // @ts-ignore
         'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
         'appium:newCommandTimeout': 240,
